e1a5b7e87cd7e4986b030467ad38244de718a735
Keith Smiley <keithbsmiley@gmail.com>
2015-08-17 12:03:23 -0700

    Update return arrow whitespace rule

--- Source/SwiftLintFramework/Rules/ReturnArrowWhitespaceRule.swift
+++ Source/SwiftLintFramework/Rules/ReturnArrowWhitespaceRule.swift
@@ -13,23 +13,16 @@ public struct ReturnArrowWhitespaceRule: Rule {
     public init() {}
 
     public let identifier = "return_arrow_whitespace"
+    private static let MatchRegex = try! NSRegularExpression(pattern: "(\\)\\s*->[^\\n|\\s]|\\)->[\\n|\\s]|\\)\\s{2,}->\\s{2,})", options: [])
 
     public func validateFile(file: File) -> [StyleViolation] {
-        // just horizontal spacing so that "func abc()->\n" can pass validation
-        let space = "[ \\f\\r\\t]"
-        let spaceRegex = "(\(space){0}|\(space){2,})"
+        let range = NSRange(location: 0, length: count(file.contents))
+        let matches = ReturnArrowWhitespaceRule.MatchRegex.matchesInString(file.contents,
+            options: nil, range: range) as? [NSTextCheckingResult] ?? []
 
-        // ex: func abc()-> Int {
-        let pattern1 = file.matchPattern("\\)\(spaceRegex)\\->\\s*\\S+",
-            withSyntaxKinds: [.Typeidentifier])
-
-        // ex: func abc() ->Int {
-        let pattern2 = file.matchPattern("\\)\\s\\->\(spaceRegex)\\S+",
-            withSyntaxKinds: [.Typeidentifier])
-
-        return (pattern1 + pattern2).map { match in
+        return matches.map { match in
             return StyleViolation(type: .ReturnArrowWhitespace,
-                location: Location(file: file, offset: match.location),
+                location: Location(file: file, offset: match.range.location),
                 severity: .Warning,
                 reason: "File should have 1 space before return arrow and return type")
         }
@@ -55,7 +48,7 @@ public struct ReturnArrowWhitespaceRule: Rule {
             "func abc() ->Int {}\n",
             "func abc()  ->  Int {}\n",
             "var abc = {(param: Int) ->Bool in }\n",
-            "var abc = {(param: Int)->Bool in }\n"
+            "var abc = {(param: Int)->Bool in }\n",
         ]
     )
 }
