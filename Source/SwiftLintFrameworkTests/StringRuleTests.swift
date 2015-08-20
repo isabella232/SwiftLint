22e8389388cea54730dd307f1f382bce7bc56103
Keith Smiley <keithbsmiley@gmail.com>
2015-08-19 17:55:04 -0700

    Add test for blankline before functions after types

--- Source/SwiftLintFrameworkTests/StringRuleTests.swift
+++ Source/SwiftLintFrameworkTests/StringRuleTests.swift
@@ -114,4 +114,10 @@ class StringRuleTests: XCTestCase {
             type: .ObjcIdentifier,
             commentDoesntViolate: false)
     }
+
+    func testBlanklineFunction() {
+        verifyRule(BlanklineFunctionRule().example,
+            type: .BlanklineFunction,
+            commentDoesntViolate: false)
+    }
 }
