071a14946cf9f893ee9d23f89bf53069e44c4a9d
Keith Smiley <keithbsmiley@gmail.com>
2015-09-30 12:10:10 -0700

    Go back to line length test defaults

--- Source/SwiftLintFrameworkTests/StringRuleTests.swift
+++ Source/SwiftLintFrameworkTests/StringRuleTests.swift
@@ -11,16 +11,17 @@ import XCTest
 
 class StringRuleTests: XCTestCase {
     func testLineLengths() {
-        let longLine = Repeat(count: 110, repeatedValue: "/").joinWithSeparator("") + "\n"
+        let longLine = Repeat(count: 100, repeatedValue: "/").joinWithSeparator("") + "\n"
         XCTAssertEqual(violations(longLine), [])
         let testCases: [(String, Int, ViolationSeverity)] = [
-            ("/", 111, .Warning),
+            ("/", 101, .Warning),
+            (Repeat(count: 101, repeatedValue: "/").joinWithSeparator(""), 201, .Error)
         ]
         for testCase in testCases {
             XCTAssertEqual(violations(testCase.0 + longLine), [StyleViolation(type: .Length,
                 location: Location(file: nil, line: 1),
                 severity: testCase.2,
-                reason: "Line should be 110 characters or less: " +
+                reason: "Line should be 100 characters or less: " +
                 "currently \(testCase.1) characters")])
         }
     }
